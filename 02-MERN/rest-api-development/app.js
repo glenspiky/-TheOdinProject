143cfbf8f2cd284d7de88ec426f18cbad3bce780
Glen Barasa <glenbarasa4@gmail.com>
2026-05-26 11:33:34 +0300

    feat: rest apis

---
+++ 02-MERN/rest-api-development/app.js
@@ -0,0 +1,92 @@
+const express = require("express");
+
+const app = express();
+
+//middleware
+
+app.use(express.json());
+let books = [
+  {
+    id: "1",
+    title: "Book 1",
+  },
+  {
+    id: "2",
+    title: "Book 2",
+  },
+  {
+    id: "3",
+    title: "Book 3",
+  },
+];
+// intri route
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to our bookstore api",
+  });
+});
+
+// get all books
+app.get("/get", (req, res) => {
+  res.json(books);
+});
+
+app.get("/get/:id", (req, res) => {
+  const book = books.find((item) => item.id === req.params.id);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({
+      message: "Book not found",
+    });
+  }
+});
+
+//add a new book
+app.post("/add", (req, res) => {
+  const newBook = {
+    id: `${books.length + 1}`,
+    title: `Book ${books.length + 1}`,
+  };
+  books.push(newBook);
+  res.status(200).json({
+    data: newBook,
+    message: "Book added succesifully",
+  });
+});
+
+// update a book
+app.put("/update/:id", (req, res) => {
+  const findBook = books.find((bookItem) => bookItem.id === req.params.id);
+
+  if (findBook) {
+    findBook.title = req.body.title || findBook.title;
+    res.status(200).json({
+      message: `Book with id ${req.params.id} succesifully`,
+      data: findBook,
+    });
+  } else {
+    res.status(404).send("not found");
+  }
+});
+
+// delete a book
+
+app.delete("/delete/:id", (req, res) => {
+  const findCurrentIndex = books.findIndex((item) => item.id === req.params.id);
+  if (findCurrentIndex !== -1) {
+    const deletedBook = books.splice(findCurrentIndex, 1);
+    res.status(200).json({
+      message: "book deleted succesifully",
+      data: deletedBook[0],
+    });
+  } else {
+    res.status(404).send("not found");
+  }
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log("Listening");
+});
